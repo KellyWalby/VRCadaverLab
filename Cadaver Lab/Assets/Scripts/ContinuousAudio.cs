409f6400c5d37cc8f0c099bd049f4809d02cb66a
jarheadwerm <47260213+jarheadwerm@users.noreply.github.com>
2019-09-11 15:23:06 -0700

    first audio test

---
+++ Cadaver Lab/Assets/Scripts/ContinuousAudio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousAudio : MonoBehaviour
+{
+    private void Awake() //while Oculus is awake
+    {
+        DontDestroyOnLoad(transform.gameObject); //keeps the audio from stopping after scene change
+    }
+}
+//retrieved from youtube.com/watch?v=BKCsH8mQ-IM
