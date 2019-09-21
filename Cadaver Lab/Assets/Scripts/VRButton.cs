3483074da3ebf94951bd7bfa13ee7557b1e1b846
jarheadwerm <jarheadwerm@gmail.com>
2019-09-20 17:38:18 -0700

    menu beginning

---
+++ Cadaver Lab/Assets/Scripts/VRButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRButton : MonoBehaviour
+{
+    public Image BackgroundImage;
+    public Color NormalColor;
+    public Color HighlightColor; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnGazeEnter()
+    {
+        BackgroundImage.color = HighlightColor;
+    }
+
+    public void onGazeExit()
+    {
+        BackgroundImage.color = NormalColor;
+    }
+}
