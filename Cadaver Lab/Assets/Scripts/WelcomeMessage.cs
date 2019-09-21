d611b78795014b06c29006d59a3c95630f7ad8c1
jarheadwerm <jarheadwerm@gmail.com>
2019-09-21 11:24:57 -0700

    update

---
+++ Cadaver Lab/Assets/Scripts/WelcomeMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WelcomeMessage : MonoBehaviour
+{
+    [SerializeField] private Image myImage; //SerializeField allows option to show in the inspector; named the image I will use myImage
+
+    void OnTriggerEnter(Collider other) //when player encounters the trigger
+    {
+        if (other.CompareTag("Player")) //my camera tagged as player will initiate this function
+        {
+            myImage.enabled = true; //image will appear
+        }
+    }
+
+    void OnTriggerExit(Collider other) //when player leaves the trigger
+    {
+        if (other.CompareTag("Player")) //my camera tagged as player will initiate this function
+        {
+            myImage.enabled = false; //image will disappear
+        }
+    }
+}
+
+//script created with https://www.youtube.com/watch?v=UVUMqss4A34
